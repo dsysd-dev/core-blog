627ab987936d4957fa7faec5fff38ce8130efb51
AKASH RANJAN <akashbitm787@gmail.com>
2023-11-13 21:04:02 +0530

    add side-nav for desktop and mobile view

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,11 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Image from "next/image";
 import Link from 'next/link'
+import Header from '@/components/header';
+import HeaderMobile from '@/components/header-mobile';
+import MarginWidthWrapper from '@/components/margin-width-wrapper';
+import PageWrapper from '@/components/page-wrapper';
+import SideNav from '@/components/side-nav';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -38,11 +43,16 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body>
-        <div className="mx-auto px-6 max-w-2xl">
-          {header}
-          {children}
-          {footer}
+      <body className={`bg-white${inter.className}`}>
+        <div className="flex">
+          <SideNav />
+          <main className="flex-1">
+            <MarginWidthWrapper>
+              <Header />
+              <HeaderMobile />
+              <PageWrapper>{children}</PageWrapper>
+            </MarginWidthWrapper>
+          </main>
         </div>
       </body>
     </html>
