3609aafa776523e0508a90e7c829d529cb34e545
Anni <anni@Annis-MacBook-Pro.local>
2024-08-29 23:48:11 +0530

    npm build works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,13 +51,16 @@ const LandingPage = () => {
         )}
       >
         <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
-          <Medal className="h-6 w-6 mr-2" /> Welcome No 1 tech channel
+          <Medal className="h-6 w-6 mr-2" /> No. 1 blockchain tech channel
         </div>
         <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
-          {siteConfig.name} helps team move
+          {siteConfig.name} is your portal to learn
         </h1>
         <div className="text-3xl md:text-6xl bg-gradient-to-r from-indigo-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
-          work forward.
+          distributed systems
+        </div>
+        <div className="text-3xl md:text-6xl bg-gradient-to-r from-indigo-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
+          blockchain
         </div>
       </div>
       <div
@@ -66,9 +69,8 @@ const LandingPage = () => {
           textFont.className
         )}
       >
-        Collaborate, manage articles, share your knowledge and reach new
-        productivity peaks. From high rises to home office, the way your team
-        works is unique - accomplish it all with {siteConfig.name}.
+        Dive deep into complex topics, access curated resources, and join a community of learners passionate about cutting-edge technologies
+        Whether you&apos;re a beginner or an expert, {siteConfig.name} provides the tools and knowledge to master distributed systems and blockchain concepts
       </div>
       <Button className="mt-6" size="lg" asChild>
         <Link href="/posts"> Get Started </Link>
--- src/components/command-menu.tsx
+++ src/components/command-menu.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 import { useRouter } from 'next/navigation';
-import { DialogProps } from '@radix-ui/react-alert-dialog';
+import * as AlertDialog from '@radix-ui/react-alert-dialog';
 import {
   CircleIcon,
   FileIcon,
@@ -25,7 +25,7 @@ import {
   CommandSeparator,
 } from '@/components/ui/command';
 
-export function CommandMenu({ ...props }: DialogProps) {
+export function CommandMenu({ ...props }: AlertDialog.AlertDialogProps) {
   const router = useRouter();
   const [open, setOpen] = React.useState(false);
   const { setTheme } = useTheme();
